3e74778e133f0d5af52a684e90f5788390f416a2
satyanash <satyajeet@kanetkar.co.in>
2020-04-19 00:21:08 +0530

    remove unused theme packages

--- emacs/main.el
+++ emacs/main.el
@@ -39,7 +39,7 @@
  '(nyan-mode t)
  '(package-selected-packages
    (quote
-    (evil-collection helm-projectile lsp-origami origami ag color-theme-modern nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode gruvbox-theme monokai-theme dracula-theme neotree evil))))
+    (evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
