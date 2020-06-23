49f6c3905474262f2156f07f3966e0184ae8d015
satyanash <satyajeet@kanetkar.co.in>
2020-06-23 22:54:05 +0530

    emacs: configure basic clojure mode

--- emacs/main.el
+++ emacs/main.el
@@ -37,7 +37,7 @@
  '(nyan-mode t)
  '(package-selected-packages
    (quote
-    (ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode lsp-java toml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil))))
+    (evil-smartparens ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode lsp-java toml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -240,5 +240,11 @@
 (progn ; configure org-mode
   (setq org-hierarchical-todo-statistics nil
 	org-checkbox-hierarchical-statistics nil))
+
+(progn ; configure Clojure env
+  (require 'smartparens-config)
+  (add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+  (add-hook 'clojure-mode-hook #'evil-smartparens-mode)
+  (add-hook 'clojure-mode-hook #'cider-mode))
 
 (provide 'main)
