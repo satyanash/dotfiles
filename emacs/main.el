acc4eeb5071713166f67aa315d0a96a83bef534a
satyanash <satyajeet@kanetkar.co.in>
2020-07-31 22:44:12 +0530

    emacs: fix typo in package-selected-packages

--- emacs/main.el
+++ emacs/main.el
@@ -40,7 +40,7 @@
  '(nyan-mode t)
  '(package-selected-packages
    (quote
-    (dockerfile-mode evil-smartparens ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode oml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil)))
+    (dockerfile-mode evil-smartparens ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode toml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
