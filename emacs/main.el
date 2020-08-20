71e54cc85d1543df4f466639b72b7a9dfc9bb04e
satyanash <satyajeet@kanetkar.co.in>
2020-08-20 23:51:35 +0530

    emacs: fix font line that keeps getting removed

--- emacs/main.el
+++ emacs/main.el
@@ -54,8 +54,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; Get "Monego" from https://github.com/cseelus/monego
  '(default ((t (:height 150 :family "Monego")))))
+ ;; Get "Monego" from https://github.com/cseelus/monego
 
 (progn
     (require 'package)
