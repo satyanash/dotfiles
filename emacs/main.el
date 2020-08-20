853bcf6c16f93b6a226e691853c33cd626b9fedb
satyanash <satyajeet@kanetkar.co.in>
2020-08-20 22:16:47 +0530

    emacs: add leetcode and go-playground

--- emacs/main.el
+++ emacs/main.el
@@ -43,7 +43,7 @@
  '(nyan-mode t)
  '(package-selected-packages
    (quote
-    (eyebrowse dockerfile-mode evil-smartparens ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode toml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil)))
+    (leetcode go-playground eyebrowse dockerfile-mode evil-smartparens ranger smartparens cider clojure-mode-extra-font-locking jsonnet-mode toml-mode package-lint helm evil-collection helm-projectile lsp-origami origami ag nyan-mode magit evil-tabs terraform-mode yaml-mode nov writeroom-mode json-mode markdown-mode golden-ratio mode-line-bell helm-ag projectile lsp-mode flycheck go-mode neotree evil)))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(writeroom-fullscreen-effect (quote maximized))
