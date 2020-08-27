407d2d92397631dfdf79a6e23af530d07f8f1032
satyanash <satyajeet@kanetkar.co.in>
2020-08-28 01:03:47 +0530

    emacs: add profiling code to diagnostics

--- emacs/diagnostics.el
+++ emacs/diagnostics.el
@@ -1,13 +1,16 @@
-(defun test-file-open-timing (file-path)
+(defun satyanash--test-file-open-timing (file-path)
   (let ((before-file-load (current-time)))
+    (profiler-start 'cpu+mem)
     (find-file file-path)
+    (profiler-stop)
     (message "%s took %f seconds to open."
              file-path
              (time-to-seconds (time-since before-file-load)))
-    (kill-buffer)))
+    (kill-buffer)
+    (profiler-report)))
 
 (setq inhibit-compacting-font-caches t)
 
-(test-file-open-timing "./babel.org")
+(satyanash--test-file-open-timing "./babel.org")
 
-(test-file-open-timing "./main.el")
+(satyanash--test-file-open-timing "./main.el")
