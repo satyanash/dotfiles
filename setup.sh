b0decaf510056a23abc5804cc45f75c2bf79b7ba
satyanash <satyajeet@kanetkar.co.in>
2021-05-23 11:20:43 +0530

    fix README to use stow

--- setup.sh
+++ setup.sh
@@ -4,10 +4,3 @@ cat <<EOF > "${HOME}/.emacs"
 (add-to-list 'load-path "$(pwd)/emacs")
 (require 'main)
 EOF
-
-ln -s "$(realpath '.bashrc')" "${HOME}/.bashrc"
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ln -s "$(realpath '.i3status.conf')" "${HOME}/.i3status.conf"
-    ln -s "$(realpath '.i3')" "${HOME}/.i3"
-fi
