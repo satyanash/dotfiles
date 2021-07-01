250a6af07992761650be6bf625c757a03097c4cd
satyajeet <satyajeet@kanetkar.co.in>
2021-07-01 22:46:32 +0530

    add hammerspoon with shiftit support

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,10 @@
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall.repos.ShiftIt = {
+   url = "https://github.com/peterklijn/hammerspoon-shiftit",
+   desc = "ShiftIt spoon repository",
+   branch = "master",
+}
+
+spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
+spoon.ShiftIt:bindHotkeys({})
