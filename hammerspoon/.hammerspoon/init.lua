805c35b2a46d5ed4d27c05ff7f6bbcf94e6c6a5b
satyajeet <satyajeet@kanetkar.co.in>
2023-06-21 13:19:59 +0530

    hammerspoon: punt windows between monitors

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -8,3 +8,14 @@ spoon.SpoonInstall.repos.ShiftIt = {
 
 spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
 spoon.ShiftIt:bindHotkeys({})
+
+
+hs.hotkey.bind({'alt', 'ctrl', 'cmd'}, 'n', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
